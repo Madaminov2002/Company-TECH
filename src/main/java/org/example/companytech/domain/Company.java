dcd0f2818b7df70cac97cc7df20487687a341470
Madaminov2002 <suhrobjonmadaminov37@gmail.com>
2024-07-19 12:15:43 +0500

    1001 successfully

---
+++ src/main/java/org/example/companytech/domain/Company.java
@@ -0,0 +1,39 @@
+package org.example.companytech.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String userName;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private BigDecimal capital;
+    @ManyToMany
+    private List<Role> roles;
+}
---
+++ src/main/java/org/example/companytech/repo/CompanyRepository.java
@@ -0,0 +1,9 @@
+package org.example.companytech.repo;
+
+import org.example.companytech.domain.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+}
