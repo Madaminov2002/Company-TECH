e11025b627ae05a14d20d86d5f10e0acd2125359
Madaminov2002 <suhrobjonmadaminov37@gmail.com>
2024-07-19 10:38:17 +0500

    first commit

---
+++ src/main/java/org/example/companytech/CompanyTechApplication.java
@@ -0,0 +1,13 @@
+package org.example.companytech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyTechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/companytech/CompanyTechApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.companytech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
